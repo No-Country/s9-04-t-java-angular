21b8fc830e06605467a1a0450604961727905f2a
Federico <federicoburgos.dev@gmail.com>
2023-06-29 20:01:17 -0300

    second commit

--- coWorkingApp/src/app/app.component.ts
+++ coWorkingApp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faHouse } from '@fortawesome/free-solid-svg-icons'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'coWorkingApp';
+
+  faHouse = faHouse;
+
 }
