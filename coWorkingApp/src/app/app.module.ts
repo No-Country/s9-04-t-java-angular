2a7ddcbe1f6be999dbd9e324cb70e9b5f8030333
Federico <federicoburgos.dev@gmail.com>
2023-07-01 01:27:14 -0300

    Add workspace-card component import

--- coWorkingApp/src/app/app.module.ts
+++ coWorkingApp/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { HomeComponent } from './pages/home/home.component';
+import { WorkspaceCardComponent } from './components/workspace-card/workspace-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    WorkspaceCardComponent
   ],
   imports: [
     BrowserModule,
