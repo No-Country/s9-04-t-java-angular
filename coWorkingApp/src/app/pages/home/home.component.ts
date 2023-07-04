c926935d2585d9b610a57642866113bd870172f7
Federico <federicoburgos.dev@gmail.com>
2023-07-03 22:26:22 -0300

    añadido array de workspaces hardcodeado

--- coWorkingApp/src/app/pages/home/home.component.ts
+++ coWorkingApp/src/app/pages/home/home.component.ts
@@ -9,9 +9,41 @@ export class HomeComponent {
 
   workspaces = [
     {
-      title: 'titulo de espacio',
+      name: 'titulo de espacio',
+      images: [
+        'https://www.lista-office.com/fileadmin/lo/lo-business-solutions/Lista_Office_LO_B2B_Herobackground.jpg',
+        'https://static01.nyt.com/images/2021/05/02/business/00officespace8/00officespace8-superJumbo.jpg',
+        'https://media.npr.org/assets/img/2020/04/23/gettyimages-656544055_wide-65e041ed6c0f8e5feec6d3951fa3459431b05519-s1400-c100.jpg'
+      ],
+      capacidad: 15,
+      rating: 4.5,
+      price: 150
       
-    }
+    },
+    {
+      name: 'titulo de espacio',
+      images: [
+        'https://static01.nyt.com/images/2021/05/02/business/00officespace8/00officespace8-superJumbo.jpg',
+        'https://www.lista-office.com/fileadmin/lo/lo-business-solutions/Lista_Office_LO_B2B_Herobackground.jpg',
+        'https://media.npr.org/assets/img/2020/04/23/gettyimages-656544055_wide-65e041ed6c0f8e5feec6d3951fa3459431b05519-s1400-c100.jpg'
+      ],
+      capacidad: 15,
+      rating: 4.5,
+      price: 150
+
+    },
+    {
+      name: 'titulo de espacio',
+      images: [
+        'https://media.npr.org/assets/img/2020/04/23/gettyimages-656544055_wide-65e041ed6c0f8e5feec6d3951fa3459431b05519-s1400-c100.jpg',
+        'https://www.lista-office.com/fileadmin/lo/lo-business-solutions/Lista_Office_LO_B2B_Herobackground.jpg',
+        'https://static01.nyt.com/images/2021/05/02/business/00officespace8/00officespace8-superJumbo.jpg',
+      ],
+      capacidad: 15,
+      rating: 4.5,
+      price: 150
+
+    },
   ]
 
 }
