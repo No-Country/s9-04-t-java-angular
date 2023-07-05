8f5d04c7284e4f38cfaf20aef3a30b00271b18dc
Federico <federicoburgos.dev@gmail.com>
2023-07-05 16:23:24 -0300

    cambiadas imagenes de los objetos de workspaces

--- coWorkingApp/src/app/pages/home/home.component.ts
+++ coWorkingApp/src/app/pages/home/home.component.ts
@@ -28,9 +28,9 @@ export class HomeComponent {
       id: 2,
       name: 'Espacio 2',
       images: [
-        'https://static01.nyt.com/images/2021/05/02/business/00officespace8/00officespace8-superJumbo.jpg',
-        'https://www.lista-office.com/fileadmin/lo/lo-business-solutions/Lista_Office_LO_B2B_Herobackground.jpg',
-        'https://media.npr.org/assets/img/2020/04/23/gettyimages-656544055_wide-65e041ed6c0f8e5feec6d3951fa3459431b05519-s1400-c100.jpg'
+        'https://i.blogs.es/0e7127/2560_3000/1366_2000.jpeg',
+        'https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2020/05/gta-online-1943171.jpg?tf=3840x',
+        'https://www.lavanguardia.com/files/og_thumbnail/files/fp/uploads/2021/06/17/60cb034db908c.r_d.674-358-9375.jpeg'
       ],
       capacidad: 15,
       rating: [
@@ -44,9 +44,9 @@ export class HomeComponent {
       id: 3,
       name: 'Espacio 3',
       images: [
-        'https://media.npr.org/assets/img/2020/04/23/gettyimages-656544055_wide-65e041ed6c0f8e5feec6d3951fa3459431b05519-s1400-c100.jpg',
-        'https://www.lista-office.com/fileadmin/lo/lo-business-solutions/Lista_Office_LO_B2B_Herobackground.jpg',
-        'https://static01.nyt.com/images/2021/05/02/business/00officespace8/00officespace8-superJumbo.jpg',
+        'https://i.blogs.es/0c9485/the-batman-cartel/1366_2000.jpeg',
+        'https://estaticos-cdn.elperiodico.com/clip/bd2a087e-0076-4792-80e5-27e8d52955c1_alta-libre-aspect-ratio_default_0.png',
+        'https://media.revistagq.com/photos/62f612194b12f9b6048b3554/16:9/pass/the-batman-2-pelicula.jpeg',
       ],
       capacidad: 15,
       rating: [
