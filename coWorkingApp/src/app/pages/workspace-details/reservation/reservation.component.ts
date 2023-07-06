9e44cddc4f27549deca5a38c5f9a14c12986c2d7
DanielMino19 <danielmino926@gmail.com>
2023-07-06 17:24:44 -0300

    reactive form in reservation-details

--- coWorkingApp/src/app/pages/workspace-details/reservation/reservation.component.ts
+++ coWorkingApp/src/app/pages/workspace-details/reservation/reservation.component.ts
@@ -1,6 +1,6 @@
 import { ViewportScroller } from '@angular/common';
 import { Component, ElementRef, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { faCalendarCheck, faCircleCheck } from '@fortawesome/free-regular-svg-icons';
 import { faChevronDown, faEye, faEyeLowVision, faPhone, faToggleOff, faToggleOn, faUser } from '@fortawesome/free-solid-svg-icons';
 
@@ -10,8 +10,6 @@ import { faChevronDown, faEye, faEyeLowVision, faPhone, faToggleOff, faToggleOn,
   styleUrls: ['./reservation.component.css']
 })
 export class ReservationComponent {
-  
-
   faCircleCheck = faCircleCheck ;
   faPhone = faPhone;
   faUser = faUser;
@@ -22,11 +20,15 @@ export class ReservationComponent {
   faToggleOn = faToggleOn;
   faToggleOff = faToggleOff;
 
-  personalData: FormGroup;
   tipoContrasena: string = 'password';
   showPassword: boolean = false;
   Check: boolean = false;
   detailsOpen: boolean = false;
+
+  //Form
+  personalData: FormGroup;
+  saveCard: boolean = false;
+  receiveNotifications: boolean = false;
 
 
   constructor(
@@ -34,8 +36,44 @@ export class ReservationComponent {
     private viewportScroller: ViewportScroller
   ){
     this.personalData = this.formBuilder.group({
-
+      email:['',[Validators.required, Validators.email]],
+      name:['',[Validators.required]],
+      lastName:['',[Validators.required]],
+      ownerName:['',[Validators.required]],
+      cardNumber:['',[Validators.required], Validators.minLength(16)],
+      cvcCvv:['',[Validators.required], Validators.minLength(3)],
+      expiration:['',[Validators.required]],
+      saveCard: this.saveCard,
+      receiveNotifications: this.receiveNotifications
     });
+  }
+
+  get email() {
+    return this.personalData.get('email');
+  }
+  
+  get name() {
+    return this.personalData.get('name');
+  }
+  
+  get lastName() {
+    return this.personalData.get('lastName');
+  }
+  
+  get ownerName() {
+    return this.personalData.get('ownerName');
+  }
+  
+  get cardNumber() {
+    return this.personalData.get('cardNumber');
+  }
+  
+  get cvcCvv() {
+    return this.personalData.get('cvcCvv');
+  }
+  
+  get expiration() {
+    return this.personalData.get('expiration');
   }
 
   togglePasswordVisibility() {
@@ -45,6 +83,7 @@ export class ReservationComponent {
 
   toggleCheck() {
     this.Check = !this.Check;
+    this.saveCard = !this.saveCard;
   }
 
 
@@ -53,9 +92,12 @@ export class ReservationComponent {
     this.viewportScroller.scrollToAnchor('end');
   }
 
-  onSubmit(){}
 
-  onLoginFormSubmit(){}
+  onLoginFormSubmit(){
+    if (this.personalData.invalid){
+      return
+    }
+  }
 
 
 
