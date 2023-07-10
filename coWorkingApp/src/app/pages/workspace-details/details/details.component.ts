9bcf4f44a18ce37c5501f169f276fa0396a4d69b
Federico <federicoburgos.dev@gmail.com>
2023-07-10 11:33:34 -0300

    details comp comentado para que no moleste de momento

--- coWorkingApp/src/app/pages/workspace-details/details/details.component.ts
+++ coWorkingApp/src/app/pages/workspace-details/details/details.component.ts
@@ -8,18 +8,9 @@ import { CoworkService } from 'src/app/services/cowork.service';
 })
 export class DetailsComponent implements OnInit {
 
-  items = ['item 1', 'item 2', 'item 3'];
-  expandedIndex = 0;
-
-  product: any;
   coworkService = inject(CoworkService);
   
   ngOnInit(): void {
-    this.coworkService.getProductObservable().subscribe({
-      next: (res) => {
-        this.product = res;
-      }
-    });
   }
 
 
