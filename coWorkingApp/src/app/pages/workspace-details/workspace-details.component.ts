1760c2731db619d85cf3fd412410e34419f747c8
Federico <federicoburgos.dev@gmail.com>
2023-07-10 11:34:47 -0300

    tratando de consumir servicio

--- coWorkingApp/src/app/pages/workspace-details/workspace-details.component.ts
+++ coWorkingApp/src/app/pages/workspace-details/workspace-details.component.ts
@@ -3,6 +3,7 @@ import { faArrowLeft, faShareNodes, faLocationDot, faStar } from '@fortawesome/f
 import { faHeart } from '@fortawesome/free-regular-svg-icons';
 import { ActivatedRoute, Params } from '@angular/router';
 import { CoworkService } from 'src/app/services/cowork.service';
+import { Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-workspace-details',
@@ -17,61 +18,42 @@ export class WorkspaceDetailsComponent implements OnInit, OnDestroy {
   faStar = faStar;
   faHeart = faHeart;
 
-  workspace = {
-    id: 3,
-    name: 'Espacio 3',
-    images: [
-      'https://i.blogs.es/0c9485/the-batman-cartel/1366_2000.jpeg',
-      'https://estaticos-cdn.elperiodico.com/clip/bd2a087e-0076-4792-80e5-27e8d52955c1_alta-libre-aspect-ratio_default_0.png',
-      'https://media.revistagq.com/photos/62f612194b12f9b6048b3554/16:9/pass/the-batman-2-pelicula.jpeg',
-    ],
-    location: 'Avenida Buenos Aires, 2050, CABA',
-    capacidad: 15,
-    rating: [
-      4.5,
-      3,
-      4.5,
-      3,
-      2.1,
-      5,
-      4.5,
-      3.1
-    ],
-    price: 150,
-    description: 'El nuevo coworking para empresas y autónomos en el corazón del barrio de Sant Andreu, en Barcelona, con todo lo que necesitas: sala de reuniones, estudio fotográfico, servicio de impresión, espacio de descanso con café y agua, Ethernet y Wifi, y lo más importante, acceso las 24h del día.'
-  }
-
-  // tests
-
-  product: any;
-
+  workspace: any;
+  workspaceSub: any;
   id: number = 0;
 
   _activatedRoute = inject(ActivatedRoute);
   coworkService = inject(CoworkService);
 
-  interval: any;
-
   ngOnInit(): void {
+    this.getIdParam();
+    this.getSelectedWorkspace(this.id);
+  }
+
+  ngOnDestroy(): void {
+    this.workspaceSub.unsubscribe();
+    this.coworkService.selectedWorkspace.next(undefined);
+  }
+
+  getIdParam() {
     this._activatedRoute.params.subscribe({
       next: (params: Params) => {
         this.id = params['id'];
       },
       error: (error) => console.log(error),
-      complete: () => { }
-    });
-    this.coworkService.getProduct(this.id);
-    this.coworkService.getProductObservable().subscribe({
-      next: (res) => {
-        this.product = res;
-      },
-      error: (error) => {console.log(error)},
-      complete: () => { }
+      complete: () => {}
     });
   }
 
-  ngOnDestroy(): void {
-    this.coworkService.selectedProduct.next(undefined)
+  getSelectedWorkspace(id: number) {
+    this.workspaceSub = this.coworkService.getWorkspaceById(id).subscribe({
+      next: (res: any) => {
+        this.workspace = res;
+        console.log(res);
+      },
+      error: (error: any) => console.log(error),
+      complete: () => { }
+    });
   }
 
 }
