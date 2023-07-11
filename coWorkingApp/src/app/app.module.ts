e7451f4b247b5800a203fa810f4589332f833c0f
Federico <federicoburgos.dev@gmail.com>
2023-07-11 15:08:41 -0300

    fix app.module

--- coWorkingApp/src/app/app.module.ts
+++ coWorkingApp/src/app/app.module.ts
@@ -3,13 +3,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { OverlayModule } from '@angular/cdk/overlay';
-<<<<<<< HEAD
-import { FullCalendarModule } from '@fullcalendar/angular';
-=======
+// import { FullCalendarModule } from '@fullcalendar/angular';
 import { CdkAccordionModule } from '@angular/cdk/accordion';
 import { NgxSliderModule } from '@angular-slider/ngx-slider';
 import { CdkMenuModule } from '@angular/cdk/menu';
->>>>>>> 622b2e2d32e2fbc0f0c1a4d37149cf2c954e3111
 
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
@@ -23,7 +20,6 @@ import { MapComponent } from './pages/map/map.component';
 import { LoginComponent } from './pages/login/login.component';
 import { RegistroComponent } from './pages/registro/registro.component';
 import { FooterComponent } from './pages/footer/footer.component';
-import { CalendaryComponent } from './components/calendary/calendary.component';
 
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -39,23 +35,16 @@ import { AverageRatingPipe } from './pipes/average-rating.pipe';
     WorkspaceDetailsComponent,
     DetailsComponent,
     ReservationComponent,
-<<<<<<< HEAD
-// <<<<<<< HEAD
     LoginComponent,
     RegistroComponent,
-// =======
     MapComponent,
     LoginComponent,
     FooterComponent,
-    CalendaryComponent
-// >>>>>>> 51183b641e882060528cff771af2b948f09468df
-=======
     RegistroComponent,
     MapComponent,
     LoginComponent,
     WorkspaceCardComponent,
     AverageRatingPipe
->>>>>>> 622b2e2d32e2fbc0f0c1a4d37149cf2c954e3111
   ],
   imports: [
     BrowserModule,
@@ -63,16 +52,13 @@ import { AverageRatingPipe } from './pipes/average-rating.pipe';
     AppRoutingModule,
     FontAwesomeModule,
     OverlayModule,
-<<<<<<< HEAD
-    FullCalendarModule
-=======
     CdkAccordionModule,
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
     NgxSliderModule,
-    CdkMenuModule
->>>>>>> 622b2e2d32e2fbc0f0c1a4d37149cf2c954e3111
+    CdkMenuModule,
+    // FullCalendarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
