539c12b27d4d76b5a2a847eebc9020eda941b1a9
DanielMino19 <danielmino926@gmail.com>
2023-07-11 21:01:40 -0300

    added interface months for schedule component

---
+++ coWorkingApp/src/app/interfaces/months.ts
@@ -0,0 +1,21 @@
+export interface Month {
+    number: number;
+    name: string;
+    days: number;
+  }
+  
+  export const months: Month[] = [
+    { number: 1, name: 'Enero', days: 31 },
+    { number: 2, name: 'Febrero', days: 28 },
+    { number: 3, name: 'Marzo', days: 31 },
+    { number: 4, name: 'Abril', days: 30 },
+    { number: 5, name: 'Mayo', days: 31 },
+    { number: 6, name: 'Junio', days: 30 },
+    { number: 7, name: 'Julio', days: 31 },
+    { number: 8, name: 'Agosto', days: 31 },
+    { number: 9, name: 'Septiembre', days: 30 },
+    { number: 10, name: 'Octubre', days: 31 },
+    { number: 11, name: 'Noviembre', days: 30 },
+    { number: 12, name: 'Diciembre', days: 31 },
+  ];
+  
