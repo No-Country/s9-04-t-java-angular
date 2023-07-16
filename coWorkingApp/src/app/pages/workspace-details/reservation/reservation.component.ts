187639cc55229dae53439f625e9c19b7d1729c6c
DanielMino19 <danielmino926@gmail.com>
2023-07-15 23:33:35 -0300

    change status detailsOpen

--- coWorkingApp/src/app/pages/workspace-details/reservation/reservation.component.ts
+++ coWorkingApp/src/app/pages/workspace-details/reservation/reservation.component.ts
@@ -39,7 +39,7 @@ export class ReservationComponent implements OnInit {
   tipoContrasena: string = 'password';
   showPassword: boolean = false;
   CheckSaveCard: boolean = false;
-  detailsOpen: boolean = true;
+  detailsOpen: boolean = false;
 
   //Form
   formErrors: boolean = false;
