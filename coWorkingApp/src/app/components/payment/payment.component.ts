edb452bf5e4245fdb2019ba509167d2fc3684be5
DanielMino19 <danielmino926@gmail.com>
2023-07-18 23:44:06 -0300

    change styles from payment and fix some styles focus in reservation

--- coWorkingApp/src/app/components/payment/payment.component.ts
+++ coWorkingApp/src/app/components/payment/payment.component.ts
@@ -18,35 +18,25 @@ export class PaymentComponent implements OnInit {
   faUser = faUser;
   @ViewChild(StripeCardComponent) card: StripeCardComponent;
 
-
-
   cardOptions: StripeCardElementOptions = {
     style: {
-     
       base:{
         color: '#31325F',
         fontWeight: '300',
         fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
         fontSize: '18px',
         '::placeholder': {
-          color: '#CFD7E0'
+          color: '#CFD7E0',
         },
-       
       },
-      
     },
     classes:{
      base: 'stripe',
-
     },
-
   };
-
-
 
   elementsOptions: StripeElementsOptions = {
     locale: 'es',
-    
   };
 
   stripeTest: FormGroup;
