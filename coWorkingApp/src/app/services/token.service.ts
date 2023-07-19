ca3ca47ef5ed969d7c92be54bcba4e4431f75ba2
Elcira Ibarra <elcira.rpi@gmail.com>
2023-07-18 21:27:29 -0300

    Creando servicio de token

---
+++ coWorkingApp/src/app/services/token.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenService {
+
+  constructor(
+    private router: Router
+  ) { }
+
+  saveToken(token: string) {
+    localStorage.setItem('token', token);
+  }
+
+  getToken(): string {
+    return localStorage.getItem('token');
+  }
+
+  clearToken() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
+}
