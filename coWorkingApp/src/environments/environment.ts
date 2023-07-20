6d7fe0685fb6afee1d290560b21aa5e635c38841
Elcira Ibarra <elcira.rpi@gmail.com>
2023-07-20 13:58:42 -0300

    Actualizando los environments con las credendiales de Supabase

--- coWorkingApp/src/environments/environment.ts
+++ coWorkingApp/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  API_URL: 'https://woco-production.up.railway.app'
+  supabaseUrl: 'https://yhhcifsgfjyrxhnitiwq.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InloaGNpZnNnZmp5cnhobml0aXdxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODk4MjMyODMsImV4cCI6MjAwNTM5OTI4M30.8gb6WKpZv3NvT24QbA-qvfM6-VpswOBy6SLnXLNd1zQ'
 };
