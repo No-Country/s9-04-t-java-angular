c74fd4fdb31db2d6a41b161dc5f5e9a93d88b1de
Elcira Ibarra <elcira.rpi@gmail.com>
2023-07-20 13:57:10 -0300

    Actualizando lógica del guardián

--- coWorkingApp/src/app/guards/token.guard.ts
+++ coWorkingApp/src/app/guards/token.guard.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { TokenService } from '../services/token.service';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,22 +10,19 @@ import { TokenService } from '../services/token.service';
 export class TokenGuard implements CanActivate {
 
   constructor(
-    private tokenService: TokenService,
+    private authService: AuthService,
     private router: Router
   ) {}
 
   canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  route: ActivatedRouteSnapshot,
+  state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 
-    const token = this.tokenService.getToken();
-    if (!token) {
-      this.router.navigate(['/login']);
+    if (!this.authService.isLoggedIn()) {
       return true;
     } else {
-      this.router.navigate(['/reservation']);
+      this.router.navigate(['/login']);
       return false;
     }
   }
-
 }
