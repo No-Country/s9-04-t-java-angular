a11c53cfefe8576f5faca62fb26eac1dc593a86d
DanielMino19 <danielmino926@gmail.com>
2023-07-24 19:17:31 -0300

    off guard

--- coWorkingApp/src/app/guards/token.guard.ts
+++ coWorkingApp/src/app/guards/token.guard.ts
@@ -17,8 +17,7 @@ export class TokenGuard implements CanActivate {
   canActivate(
   route: ActivatedRouteSnapshot,
   state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-
-    if (!this.authService.isLoggedIn()) {
+    if (!!this.authService.isLoggedIn()) {
       return true;
     } else {
       this.router.navigate(['/login']);
