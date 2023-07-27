24457f578c4d38810aee59ff5f45106fd3097b43
Federico <federicoburgos.dev@gmail.com>
2023-07-26 21:00:08 -0300

    logica para la navbar en base a si hay o no usuario iniciado

--- coWorkingApp/src/app/app.component.ts
+++ coWorkingApp/src/app/app.component.ts
@@ -1,24 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
 import { faHouse, faBars, faXmark } from '@fortawesome/free-solid-svg-icons'
+import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   
   title = 'coWorkingApp';
 
   navbarMenu: boolean = false;
 
+  authService = inject(AuthService);
+  user: any;
+
   faHouse = faHouse;
   faBars = faBars;
   faXmark = faXmark;
 
+  ngOnInit(): void {
+    this.authService.getUser();
+    this.getUserObservable();
+  }
+
   toggleNavbar() {
     this.navbarMenu = !this.navbarMenu;
     document.body.classList.toggle('hidden-scroll');
+  }
+
+  logOut() {
+    this.authService.logout();
+  }
+
+  getUserObservable() {
+    this.authService.getUserObservable().subscribe({
+      next: (data) => {
+        this.user = data;
+      }
+    });
   }
 
 }
